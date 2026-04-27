34141cf393140de6067de1552f9b4506d8f0d260
punkbug <punkbug@gmail.com>
2026-04-27 12:59:59 +0900

    Initial Commit

---
+++ SLJJUK/SLJJUKApp.swift
@@ -0,0 +1,17 @@
+//
+//  SLJJUKApp.swift
+//  SLJJUK
+//
+//  Created by 김현일 on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct SLJJUKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SLJJUKTests/SLJJUKTests.swift
@@ -0,0 +1,17 @@
+//
+//  SLJJUKTests.swift
+//  SLJJUKTests
+//
+//  Created by 김현일 on 4/27/26.
+//
+
+import Testing
+@testable import SLJJUK
+
+struct SLJJUKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
